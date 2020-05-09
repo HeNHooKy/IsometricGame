9d13d2423ea2d53186eea409a918ec27bb3d7e9d
HaNJI <henhooky@gmail.com>
2020-05-09 23:12:02 +0500

    Мишки-гамми

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject player;
+    
+    public 
+
+
+    
+
+    void Update()
+    {
+        
+        
+    }
+
+
+    
+
+    
+}
